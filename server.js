560aa528d86e358d3ce2334924a29a6c67a971c4
Onwuzor Victor <onwuzorvictor.com>
2018-11-30 17:33:25 +0100

    add server config for heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // send the user to index html page inspite of the url
 app.use('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist/index.html'));
+  res.sendFile(path.resolve(__dirname, 'build/index.html'));
 });
 
 app.listen(port, () => {
